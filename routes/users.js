fe1ce2429c9d31a2455941af3000cf0c77b32a76
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-06-28 22:37:02 -0300

    Rota de usuarios e endpoint e correcao de importacao

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getUsers } from "../controllers/user.js";
+import { getUsers } from "../controllers/user.js"
 
 const router = express.Router();
 
-router.get("/". getUsers);
+router.get("/", getUsers);
 
 export default router;
